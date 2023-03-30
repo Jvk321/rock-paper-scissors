59bb5bab307de90573610f10b8faa6c130472210
maurycy <maurycygorski1212@gmail.com>
2023-03-30 16:01:58 +0200

    Added a prompt for user, Made player selection case-insensitive so users can input rock, ROCK, RocK or any other variation.

--- game.js
+++ game.js
@@ -1,30 +1,30 @@
-const option = ['Rock', "Paper", "Scissors"]
+const option = ['rock', "paper", "scissors"]
 function getComputerChoice(option) {
     optionIndex = Math.floor(Math.random() * option.length);
     let item = option[optionIndex];
     return item;
 }
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock" && computerSelection == "Paper"){
+    if (playerSelection == "rock" && computerSelection == "paper"){
         console.log("You Lose! Paper beats Rock")
     } 
-    else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+    else if (playerSelection == "scissors" && computerSelection == "rock"){
         console.log("You Lose! Rock beats Scissors")
     } 
-    else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+    else if (playerSelection == "paper" && computerSelection == "scissors"){
         console.log("You Lose! Rock beats Scissors")
     }
-    else if (playerSelection == "Paper" && computerSelection == "Rock"){
+    else if (playerSelection == "paper" && computerSelection == "rock"){
         console.log("You Win! Paper beats Rock")
     }
-    else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+    else if (playerSelection == "rock" && computerSelection == "scissors"){
         console.log("You Win! Rock beats Scissors")
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+    else if (playerSelection == "scissors" && computerSelection == "paper"){
         console.log("You Win! Scissors beats Paper")
     }
     else console.log("It's a Tie!")
 }
-const playerSelection = "Rock";
+const playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
 const computerSelection = getComputerChoice(option);
 console.log(playRound(playerSelection, computerSelection));
