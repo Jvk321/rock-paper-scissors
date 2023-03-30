ae628a01e518963444b610657eb86f72e9480607
maurycy <maurycygorski1212@gmail.com>
2023-03-30 13:04:36 +0200

    Created logic to game

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
 const option = ['Rock', "Paper", "Scissors"]
 function getComputerChoice(option) {
-    randomIndex = Math.floor(Math.random() * option.length);
-    let item = option[randomIndex];
+    optionIndex = Math.floor(Math.random() * option.length);
+    let item = option[optionIndex];
     return item;
 }
+console.log(getComputerChoice(option));
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper"){
+        console.log("You Lose! Paper beats Rock")
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        console.log("You Lose! Rock beats Scissors")
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        console.log("You Lose! Rock beats Scissors")
+    }else if (playerSelection == "Paper" && computerSelection == "Rock"){
+        console.log("You Win! Paper beats Rock")
+    }else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        console.log("You Win! Rock beats Scissors")
+    }else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        console.log("You Win! Scissors beats Paper")
+    }else console.log("It's a Tie!")
+}
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice(option);
+console.log(playRound(playerSelection, computerSelection));
